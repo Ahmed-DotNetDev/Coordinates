2ee075e47015bf1c585f53684d8311b15b70c4a3
Ahmed.net <Ahmed.net@gmail.com>
2023-03-23 00:37:49 +0200

    Adding project

---
+++ Coordinates/app.js
@@ -0,0 +1,11 @@
+function clearCoor() {
+    document.getElementById("demo").innerHTML = "";
+  }
+
+  function myFunction(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+    let coor = "Coordinates: (" + x + "," + y + ")";
+    document.getElementById("demo").innerHTML = coor ;
+    
+  }
